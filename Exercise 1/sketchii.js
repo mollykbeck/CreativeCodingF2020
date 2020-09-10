da7a28c60e3bf805b322bfefcf8bfa32b3bcdbe3
Molly Killingbeck <70911117+mollykbeck@users.noreply.github.com>
2020-09-10 19:16:25 -0400

    get rid of stroke on rectangles

--- Exercise 1/sketchii.js
+++ Exercise 1/sketchii.js
@@ -16,11 +16,13 @@ function draw() {
   background(245,66,239);
 
   fill(245,114,66);
+  stroke(245,66,239);
   rect(xpos,xpos,175);
   xpos = xpos + 5;
   ypos = ypos + 5;
 
   fill(2,137,158);
+  stroke(245,66,239);
   rect(xpos2,ypos2,175);
   xpos2 = xpos2 - 5;
   ypos2 = ypos2 - 5;
