9f4a91b05a8e94d20b0dc97accc70e08f7f0ac0a
Molly Killingbeck <70911117+mollykbeck@users.noreply.github.com>
2020-09-19 12:33:58 -0400

    tidy up

--- Inclassassignment2/sketchiii.js
+++ Inclassassignment2/sketchiii.js
@@ -7,7 +7,6 @@ let yballspeed = 1.5;
 function setup() {
 
 createCanvas(windowWidth,windowHeight);
-
 }
 
 function draw() {
@@ -19,21 +18,19 @@ function draw() {
   xpos = xpos +xballspeed;
   ypos = ypos +yballspeed;
 
-  //check for x position
   if(xpos > width){
     xballspeed = -xballspeed;
   }
   if(xpos <0){
     xballspeed = -xballspeed
   }
-  //check for y position
+
   if(ypos > height){
     yballspeed = -yballspeed
   }
   if(ypos <0){
     yballspeed = -yballspeed;
   }
-
   if(mouseIsPressed & dist(xpos,ypos,mouseX,mouseY <35)){
       xpos = random(windowWidth,windowHeight);
   }
