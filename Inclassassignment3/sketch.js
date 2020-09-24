28383e761bef9ef29fcf948d310a46dadb48214a
Molly Killingbeck <70911117+mollykbeck@users.noreply.github.com>
2020-09-24 14:49:59 -0400

    correct spacing alter varaibles no fill included

--- Inclassassignment3/sketch.js
+++ Inclassassignment3/sketch.js
@@ -7,8 +7,10 @@ function setup() {
 
 function draw() {
   //Put draw code here
-for(let i = 0; i <width; i = i + 20){
+for(let m=0; m < width; m = m + 20){
+  for(let k=0; k < height; k = k + 20){
     nofill();
-    rect(i,j,19);
+    rect(m,k,19);
+  }
   }
 }
