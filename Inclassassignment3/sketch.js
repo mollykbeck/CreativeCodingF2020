526a7c4793bcb5a35b81dca6f3b49615061d303f
Molly Killingbeck <70911117+mollykbeck@users.noreply.github.com>
2020-09-24 15:44:12 -0400

    experiment

--- Inclassassignment3/sketch.js
+++ Inclassassignment3/sketch.js
@@ -17,4 +17,12 @@ function draw() {
       triangle(m,k,10,m,20,20);
     }
   }
+  for(let m=0; m < width; m = m + 100){
+    for(let k=300; k < height; k = k + 100){
+      let rcol = random(0,255);
+      let gcol = random(0,255);
+      let bcol = random(0,255);
+      stroke(rcol,gcol,bcol);
+      fill(rcol,gcol,bcol);
+      triangle(m,k,250,m,300,300);
 }
