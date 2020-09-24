17fc84fded92d8be4f0a03edbd08c7b9bb00613c
Molly Killingbeck <70911117+mollykbeck@users.noreply.github.com>
2020-09-24 14:57:32 -0400

    clear draw refresh background

--- Inclassassignment3/sketch.js
+++ Inclassassignment3/sketch.js
@@ -3,16 +3,8 @@ function setup() {
 
   createCanvas(windowWidth,windowHeight);
   background(255,242,251);
-
-  for(let m=0; m < width; m = m + 20){
-    for(let k=0; k < height; k = k + 20){
-      nofill();
-      rect(m,k,19);
-    }
-    }
 }
 
 function draw() {
   //Put draw code here
-
 }
