e54fed3472f4e9ec1a2c7699aa3651993430e948
Molly Killingbeck <70911117+mollykbeck@users.noreply.github.com>
2020-09-24 14:18:40 -0400

    grid

--- Inclassassignment3/sketch.js
+++ Inclassassignment3/sketch.js
@@ -1,14 +1,14 @@
 
 function setup() {
-  // put setup code here
+
   createCanvas(windowWidth,windowHeight);
   background(30,0,0);
-
 }
 
 function draw() {
   //Put draw code here
-
-
+for(let i = 0; i <width; i = i + 20){
+  rect(i,0,20);
+}
 
 }
