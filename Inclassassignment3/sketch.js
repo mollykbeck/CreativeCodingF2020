215beb16545c49c27090e19e4be5ea4d1975cb58
Molly Killingbeck <70911117+mollykbeck@users.noreply.github.com>
2020-09-24 15:52:12 -0400

    insert focal point blue-scale background removal

--- Inclassassignment3/sketch.js
+++ Inclassassignment3/sketch.js
@@ -2,21 +2,15 @@
 function setup() {
 
   createCanvas(windowWidth,windowHeight);
-  background(255,242,251);
+  background(205,204,252);
 }
 
 function draw() {
   //Put draw code here
-  for(let m=0; m < width; m = m + 20){
-    for(let k=20; k < height; k = k + 20){
-      let rcol = random(0,255);
-      let gcol = random(0,255);
-      let bcol = random(0,255);
-      stroke(255,gcol,bcol);
-      fill(255,gcol,bcol);
-      triangle(m,k,10,m,20,20);
-    }
-  }
+  stroke(0,0,0);
+  fill(0,0,0);
+  circle(300,300,100);
+
   for(let m=0; m < width; m = m + 100){
     for(let k=300; k < height; k = k + 100){
       let rcol = random(0,255);
