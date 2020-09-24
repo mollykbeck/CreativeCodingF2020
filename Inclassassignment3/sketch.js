950d20f1fd961fa008c8c470c6420b09c217b314
Molly Killingbeck <70911117+mollykbeck@users.noreply.github.com>
2020-09-24 14:40:25 -0400

    no fill

--- Inclassassignment3/sketch.js
+++ Inclassassignment3/sketch.js
@@ -9,7 +9,8 @@ function draw() {
   //Put draw code here
 for(let i = 0; i <width; i = i + 20){
   for(let j = 0; j <height; j = j + 20){
-      rect(i,j,19);
+    nofill();
+    rect(i,j,19);
   }
 }
 
