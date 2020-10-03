ee63bb93fcaa353845efe1f79c1c3588195358f6
Molly Killingbeck <70911117+mollykbeck@users.noreply.github.com>
2020-10-03 13:35:38 -0400

    reorder

--- Project1/sketch.js
+++ Project1/sketch.js
@@ -3,15 +3,7 @@ let xpos = 300
 let ypos = 200
 
 function setup() {
-  // create full-screen canvas
-  createCanvas(windowWidth,windowHeight);
-  //keep background color red-orange
-  background(255,83,73);
-  //initial cluster of circles (3)
-  fill(255,160,137);
-  stroke(255,164,116);
-  circle(xpos,ypos,100);
-  
+
   if(mouseIsPressed & dist(xpos,ypos,mouseX,mouseY <50)){
     fill(255,164,116);
     stroke(255,160,137);
@@ -21,6 +13,15 @@ function setup() {
     stroke(255,164,116);
     circle(350,175,45);
     }
+  // create full-screen canvas
+  createCanvas(windowWidth,windowHeight);
+  //keep background color red-orange
+  background(255,83,73);
+  //initial cluster of circles (3)
+  fill(255,160,137);
+  stroke(255,164,116);
+  circle(xpos,ypos,100);
+
 }
 
 function draw() {
