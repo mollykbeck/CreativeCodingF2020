40fc95649a60274809618bca58008cf06429fd88
Molly Killingbeck <70911117+mollykbeck@users.noreply.github.com>
2020-10-03 12:16:05 -0400

    circle color trial 2

--- Project1/sketch.js
+++ Project1/sketch.js
@@ -9,8 +9,8 @@ function draw() {
   //keep background color red-orange
 background(255,83,73);
 //draw initial circle
-fill(255,164,116);
-stroke(255,160,137);
+fill(255,160,137);
+stroke(255,164,116);
 circle(300,200,50);
 
 
