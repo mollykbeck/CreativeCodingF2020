eee372f16f82536ca9d4ed85e84448243b265ba9
Molly Killingbeck <70911117+mollykbeck@users.noreply.github.com>
2020-10-03 13:18:26 -0400

    first attempt mouseIsPressed

--- Project1/sketch.js
+++ Project1/sketch.js
@@ -1,4 +1,6 @@
 
+let xpos =
+
 function setup() {
   // create full-screen canvas
 createCanvas(windowWidth,windowHeight);
@@ -8,17 +10,20 @@ createCanvas(windowWidth,windowHeight);
 function draw() {
   //keep background color red-orange
 background(255,83,73);
-//draw initial circle
+//initial cluster of circles (3)
 fill(255,160,137);
 stroke(255,164,116);
 circle(300,200,100);
 
-fill(255,164,116);
-stroke(255,160,137);
-circle(350,250,75);
+if(mouseIsPressed & dist(300,200,mouseX,mouseY <50)){
+  fill(255,164,116);
+  stroke(255,160,137);
+  circle(350,250,75);
 
-fill(253,217,181);
-stroke(255,164,116);
-circle(350,175,45);
+  fill(253,217,181);
+  stroke(255,164,116);
+  circle(350,175,45);
+}
+
 
 }
