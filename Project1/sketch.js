7dca0b27b79c7de4e050b2200536639599660a61
Molly Killingbeck <70911117+mollykbeck@users.noreply.github.com>
2020-10-03 12:26:12 -0400

    color variables

--- Project1/sketch.js
+++ Project1/sketch.js
@@ -1,4 +1,11 @@
 
+let atomic = (255,164,116);
+let vivid = (255,160,137);
+let sienna = (234,126,93);
+let apricot = (253,217,181);
+
+
+
 function setup() {
   // create full-screen canvas
 createCanvas(windowWidth,windowHeight);
@@ -9,8 +16,8 @@ function draw() {
   //keep background color red-orange
 background(255,83,73);
 //draw initial circle
-fill(255,160,137);
-stroke(255,164,116);
+fill(vivid);
+stroke(atomic);
 circle(300,200,50);
 
 
