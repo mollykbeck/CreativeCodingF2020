42b606b36916d88fdc7e514fa1806d50a8d69a7f
Molly Killingbeck <70911117+mollykbeck@users.noreply.github.com>
2020-10-03 11:43:11 -0400

    Background: Red Orange

--- Project1/sketch.js
+++ Project1/sketch.js
@@ -1,15 +1,14 @@
 
 function setup() {
-  // put setup code here
-createCanvas(200,200);
-background(200,10,120);
+  // create full-screen canvas
+createCanvas(windowWidth,windowHeight);
 
 }
 
 function draw() {
-  //Put draw code here
-  fill(30,40,200);
-  circle(100,100,70);
+  //keep background color consistent
+background(255,83,73);
+
 
 
 }
