120ff462a86656da2446dd99f06ae802e551b300
Molly Killingbeck <70911117+mollykbeck@users.noreply.github.com>
2020-10-03 12:29:06 -0400

    color check

--- Project1/sketch.js
+++ Project1/sketch.js
@@ -16,8 +16,8 @@ function draw() {
   //keep background color red-orange
 background(255,83,73);
 //draw initial circle
-fill(apricot);
-stroke(atomic);
+fill(255,160,137);
+stroke(255,164,116);
 circle(300,200,50);
 
 
