1098b4965bf279b632e856d7edff89cf4580065a
Molly Killingbeck <70911117+mollykbeck@users.noreply.github.com>
2020-10-05 13:43:51 -0400

    move background to draw

--- Project1/sketch.js
+++ Project1/sketch.js
@@ -1,16 +1,18 @@
 
+let xpos = 300
+let ypos = 200
+
+
 function setup() {
   // create full-screen canvas
 createCanvas(windowWidth,windowHeight);
-  //keep background color red-orange
-  background(255,83,73);
 
   //initial cluster of circles (3)
   fill(255,160,137);
   stroke(255,164,116);
   circle(300,200,100);
 
-if(mouseIsPressed & dist(300,200,mouseX,mouseY <50)){
+if(mouseIsPressed & dist(xpos,ypos,mouseX,mouseY <50)){
   fill(255,164,116);
   stroke(255,160,137);
   circle(350,250,75);
@@ -22,5 +24,6 @@ if(mouseIsPressed & dist(300,200,mouseX,mouseY <50)){
 }
 
 function draw() {
-
+  //keep background color red-orange
+  background(255,83,73);
 }
