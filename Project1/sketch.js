ee5442db2b58054105b3a427f47c76716d1941c5
Molly Killingbeck <70911117+mollykbeck@users.noreply.github.com>
2020-10-05 13:50:33 -0400

    remove varaibles

--- Project1/sketch.js
+++ Project1/sketch.js
@@ -1,7 +1,4 @@
 
-let xpos = 300
-let ypos = 200
-
 
 function setup() {
   // create full-screen canvas
@@ -13,18 +10,19 @@ function draw() {
   //keep background color red-orange
   background(255,83,73);
   //initial cluster of circles (3)
-if(mouseIsPressed & dist(xpos,ypos,mouseX,mouseY <50)){
+  fill(255,160,137);
+  stroke(255,164,116);
+  circle(300,200,100);
+
+if(mouseIsPressed & dist(300,200,mouseX,mouseY <50)){
   fill(255,164,116);
   stroke(255,160,137);
   circle(350,250,75);
 }
-if(mouseIsPressed & dist(xpos,ypos,mouseX,mouseY <50)){
+if(mouseIsPressed & dist(300,200,mouseX,mouseY <50)){
   fill(253,217,181);
   stroke(255,164,116);
   circle(350,175,45);
 }
 
-  fill(255,160,137);
-  stroke(255,164,116);
-  circle(300,200,100);
 }
