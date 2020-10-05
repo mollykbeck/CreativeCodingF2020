3aa11e8d9538f784a9da31d4368d45c87e39a7ae
Molly Killingbeck <70911117+mollykbeck@users.noreply.github.com>
2020-10-05 13:48:25 -0400

    separate mouse click conditionals

--- Project1/sketch.js
+++ Project1/sketch.js
@@ -6,24 +6,25 @@ let ypos = 200
 function setup() {
   // create full-screen canvas
 createCanvas(windowWidth,windowHeight);
-//keep background color red-orange
-background(255,83,73);
+}
+
+function draw() {
+
+  //keep background color red-orange
+  background(255,83,73);
   //initial cluster of circles (3)
 if(mouseIsPressed & dist(xpos,ypos,mouseX,mouseY <50)){
   fill(255,164,116);
   stroke(255,160,137);
   circle(350,250,75);
-
+}
+if(mouseIsPressed & dist(xpos,ypos,mouseX,mouseY <50)){
   fill(253,217,181);
   stroke(255,164,116);
   circle(350,175,45);
-  }
+}
 
   fill(255,160,137);
   stroke(255,164,116);
   circle(300,200,100);
-}
-
-function draw() {
-
 }
