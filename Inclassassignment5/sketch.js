bdd3b8d01b5656c403876f9173e7d48dd0de266e
Molly Killingbeck <70911117+mollykbeck@users.noreply.github.com>
2020-10-15 13:09:37 -0400

    first attempt at lemons falling

--- Inclassassignment5/sketch.js
+++ Inclassassignment5/sketch.js
@@ -1,15 +1,42 @@
 
+let lemonList = [];
+let xposList = [];
+let yposList = [];
+
+let yspeedList = [];
+
+let numoflemons = 30
+
+function preload(){
+
+for(let i = 0;1 < numoflemons;i = i +1){
+  lemonList[i] = loadImage('https://i.imgur.com/2nxDLRH.png');
+
+}
+
+}
+
 function setup() {
   // put setup code here
-createCanvas(200,200);
-background(200,10,120);
+createCanvas(windowWidth,windowHeight);
+for(let i = 0;i < numoflemons; i = i +1){
+  yposList[i] = 0;
+  xposList[i] = random(0,width);
+  yspeedList[i] = random(3,10);
+}
 
 }
 
 function draw() {
-  //Put draw code here
-  fill(30,40,200);
-  circle(100,100,70);
+  background(255,255,153);
+  imageMode(CENTER);
+
+for(let i = 0;i < numoflemons;i = i +1){
+  image(lemonList[i],xposList[i],yposList[i],100,100);
+
+  yposList[i] = yposList[i] + ypseedList[i];
+}
+
 
 
 }
