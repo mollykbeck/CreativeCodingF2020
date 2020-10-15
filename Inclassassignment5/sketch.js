09f3aba0efa2bccebb9df4b386ba33bf18eeb15d
Molly Killingbeck <70911117+mollykbeck@users.noreply.github.com>
2020-10-15 15:40:31 -0400

    add lime

--- Inclassassignment5/sketch.js
+++ Inclassassignment5/sketch.js
@@ -1,42 +1,44 @@
-
 let lemonList = [];
+let limeList = [];
+
 let xposList = [];
 let yposList = [];
 
 let yspeedList = [];
 
-let numoflemons = 30
+let sizeList = [];
+let fruittotals = 30
 
 function preload(){
+  for(let i = 0; i < fruittotals; i = i +1){
+    lemonList[i] = loadImage('https://i.imgur.com/vzaZa9i.png');
+    limeList[i] = loadImage('https://i.imgur.com/emA8rbf.png');
+  }
+  }
 
-for(let i = 0;1 < numoflemons;i = i +1){
-  lemonList[i] = loadImage('https://i.imgur.com/2nxDLRH.png');
-
-}
-
-}
-
-function setup() {
-  // put setup code here
+function setup(){
 createCanvas(windowWidth,windowHeight);
-for(let i = 0;i < numoflemons; i = i +1){
+for(let i = 0; i < fruittotals; i = i +1){
   yposList[i] = 0;
   xposList[i] = random(0,width);
-  yspeedList[i] = random(3,10);
+  yspeedList[i] = random(1,20);
+  sizeList[i] = 100,100
 }
-
 }
 
 function draw() {
-  background(255,255,153);
-  imageMode(CENTER);
+background(255,255,153);
+imageMode(CENTER);
 
-for(let i = 0;i < numoflemons;i = i +1){
-  image(lemonList[i],xposList[i],yposList[i],100,100);
+for(let i = 0; i < fruittotals; i = i +1){
+  image(limeList[i],xposList[i],yposList[i],sizeList[i],sizeList[i]);
 
-  yposList[i] = yposList[i] + ypseedList[i];
+  yposList[i] = yposList[i] + yspeedList[i];
 }
 
+for(let i = 0; i < fruittotals; i = i +1){
+  image(lemonList[i],xposList[i],yposList[i],sizeList[i],sizeList[i]);
 
-
+  yposList[i] = yposList[i] + yspeedList[i];
+}
 }
