3177e99c0c1696eab5425aa75875a83cc728b43f
Molly Killingbeck <mollykillingbeck@Mollys-MacBook-Pro.local>
2020-11-02 13:40:21 -0500

    jornal page included

--- Project2/sketch.js
+++ Project2/sketch.js
@@ -1,15 +1,31 @@
 
+let inkblot;
+let journal;
+
+function preload(){
+  inkblot = loadImage('https://i.imgur.com/LqDDNVn.png');
+  journal = loadImage('https://i.imgur.com/Sq1JLKA.jpg');
+}
+
 function setup() {
-  // put setup code here
-createCanvas(200,200);
-background(200,10,120);
+  // creamy background color to mimic a faded textile the inkblot may be on
+createCanvas(windowWidth,windowHeight);
+background(249,239,210);
 
 }
 
 function draw() {
-  //Put draw code here
-  fill(30,40,200);
-  circle(100,100,70);
+//insert text to create depth and texture
+image(journal,0,0);
+//moth inkblots centered in the composition
+imageMode(CENTER);
+tint(162,70,47,100);
+image(inkblot, width*1/5,height/2);
 
+tint(16,17,49,100);
+image(inkblot,width*4/5,height/2);
+//same color as background to create negative space
+tint(160,179,145,100);
+image(inkblot,width/2,height/2);
 
 }
