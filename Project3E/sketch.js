edc7b02bb0cda6dc35783c1cfbcfe1a5e8996061
mollykbeck <killin17@msu.edu>
2020-11-19 22:04:45 -0500

    spelling error

--- Project3E/sketch.js
+++ Project3E/sketch.js
@@ -2,7 +2,7 @@
 //hydrologic cycle, stage 5: evapotranspiration
 
 function setup() {
-createcanvas(windowWidth,windowHeight);
+createCanvas(windowWidth,windowHeight);
   background(72,179,250);
 
 }
