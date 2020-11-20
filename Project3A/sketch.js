a089ff38971a43fd4ea27c393c6371241e314805
mollykbeck <killin17@msu.edu>
2020-11-19 22:36:35 -0500

    cloud drawing

--- Project3A/sketch.js
+++ Project3A/sketch.js
@@ -5,10 +5,26 @@ function setup() {
 createCanvas(windowWidth,windowHeight);
   background(194,217,252);
 
+
 }
 
 function draw() {
 
+//central location of the cloud
+//cloud drawing
+imageMode(CENTER);
+stroke(255,255,255);
+fill(255,255,255);
+circle(width/2,height/2,300);
+circle(width/2,height*2/3,500);
 
+circle(width/3,height*2/5,340);
+circle(width*2/3,height*2/5,340);
+
+circle(width/4,height*2/3,400);
+circle(width*3/4,height*2/3,400);
+
+circle(width/7,height/2,400);
+circle(width*6/7,height/2,400);
 
 }
