c14a8dcad577125dd989ca73ef56cb5f7cf4cb59
mollykbeck <killin17@msu.edu>
2020-11-20 00:24:52 -0500

    water vapor

--- Project3A/sketch.js
+++ Project3A/sketch.js
@@ -1,30 +1,71 @@
 
 //hydrologic cycle, stage 1: condensation
+//variables for bezier effect
+let x1
+let y1
+let x2
+let y2
+let x3
+let y3
+let x4
+let y4
 
 function setup() {
 createCanvas(windowWidth,windowHeight);
   //faded blue color choice for cloudy sky
   background(194,217,252);
+  //central location of the cloud
+  //clouds drawing (background)
 
+  stroke(255,255,255);
+  fill(255,255,255);
+  circle(width/2,height/4,400);
+  circle(width/2,height*2/3,500);
+  circle(width/3,height*2/5,340);
+  circle(width*2/3,height*2/5,340);
+  circle(width/4,height*2/3,400);
+  circle(width*3/4,height*2/3,400);
+  circle(width/7,height/2,400);
+  circle(width*6/7,height/2,400);
+  circle(width/10,0,500);
+  circle(width/3,0,200);
+
+  offset = 0.0;
+noFill();
 
 }
 
 function draw() {
+  //bezier curvs mimic the water molecules in the form of vapor within the clouds
+x1 = noise(offset)*width;
+x2 = noise(offset +300)*width;
+x3 = noise(offset +600)*width;
+x4 = noise(offset +900)*width;
 
-//central location of the cloud
-//clouds drawing (background)
-imageMode(CENTER);
+y1 = noise(offset +300)*height;
+y2 = noise(offset +600)*height;
+y3 = noise(offset +900)*height;
+y4 = noise(offset +1100)*height;
+
+strokeWeight(1);
+stroke(194,217,252,75);
+bezier(x1,y1,x2,y2,x3,y3,x4,y4);
+
+offset = offset + 0.01;
+
+x1 = noise(offset)*width;
+x2 = noise(offset +200)*width;
+x3 = noise(offset +500)*width;
+x4 = noise(offset +800)*width;
+
+y1 = noise(offset +300)*height;
+y2 = noise(offset +600)*height;
+y3 = noise(offset +900)*height;
+y4 = noise(offset +1100)*height;
+
+strokeWeight(.5);
 stroke(255,255,255);
-fill(255,255,255);
-circle(width/2,height/4,400);
-circle(width/2,height*2/3,500);
-circle(width/3,height*2/5,340);
-circle(width*2/3,height*2/5,340);
-circle(width/4,height*2/3,400);
-circle(width*3/4,height*2/3,400);
-circle(width/7,height/2,400);
-circle(width*6/7,height/2,400);
-circle(width/10,0,500);
-circle(width/3,0,200);
+bezier(x1,y1,x2,y2,x3,y3,x4,y4);
 
+offset = offset + 0.012;
 }
--- Project3E/sketch.js
+++ Project3E/sketch.js
@@ -1,11 +1,19 @@
 
 //hydrologic cycle, stage 5: evapotranspiration
+//variables for bezier effect
+let x1
+let y1
+let x2
+let y2
+let x3
+let y3
+let x4
+let y4
 
 function setup() {
 createCanvas(windowWidth,windowHeight);
   background(72,179,250);
 
-  noLoop();
 
 
 }
@@ -27,6 +35,19 @@ circle(width/5,height/5,75);
 circle(width/3,height/5,135);
 circle(width/7,height/5,100);
 circle(width/6,height/4,100);
+
+noFill();
+
+x1 = random(width,0);
+y1 = random(0,height);
+x2 = random(width,0);
+y2 = random(0,height);
+x3 = random(width,0);
+y3 = random(0,height);
+x4 = random(width,0);
+y4 = random(0,height);
+
+bezier(x1,y1,x2,y2,x3,y3,x4,y4);
 
 
 
